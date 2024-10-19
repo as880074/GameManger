d74e19c5511474932973b722b17be9e2a6cb14a7
as880074 <as880074@gmail.com>
2024-10-20 00:00:34 +0800

    專案建置

---
+++ GameManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GameManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
